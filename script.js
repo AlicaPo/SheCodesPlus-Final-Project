ec43a4fefd7150eb78b37436df50372ff27118d7
AP <alica.pohancanikova@gmail.com>
2020-08-25 12:10:23 +0200

    update js

---
+++ script.js
@@ -0,0 +1,23 @@
+let now = new Date();
+let p = document.querySelector("today");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+let day = days [now.getDay()];
+let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  let month = months[now.getMonth()];
+  p.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
